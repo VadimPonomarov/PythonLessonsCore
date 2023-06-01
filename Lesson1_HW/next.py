ec5186ad27be41459f78e45ff980a6b0f6124da1
pvsve <pvs.versia@gmail.com>
2023-06-01 11:55:14 +0300

    Initial commit

--- Lesson1_HW/next.py
+++ Lesson1_HW/next.py
@@ -4,33 +4,45 @@
 #   - видалити усі дублікати
 #   - замінити кожне 4-те значення на 'X'
 
-lst = [22, 3,5,2,8,2,-23, 8,23,5]
+lst = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
 
 print(f'List: {lst} min {min(lst)}')
 print(f'List: {lst} без дублікатів:  {list(set(lst))}')
-print(f'List: {lst} з кожним четвертим X:  {["X" if not ((i+1)%4 or i==0) else v for i, v in enumerate(lst) ]}')
+print(f'List: {lst} з кожним четвертим X:  {["X" if not ((i + 1) % 4 or i == 0) else v for i, v in enumerate(lst)]}')
 
-#2) вивести на екран пустий квадрат з "*" сторона якого вказана як агрумент функції
+
+# 2) вивести на екран пустий квадрат з "*" сторона якого вказана як агрумент функції
 
 def square_asterics(num: int):
-   first_and_last = '* ' + ' * ' * (num-2) + ' *'
-   next = '* ' + '   ' * (num-2) + ' *'
-   for i in range(num):
-       print(first_and_last) if (i==0 or i==(num-1)) else print(next)
+    first_and_last = '* ' + ' * ' * (num - 2) + ' *'
+    next = '* ' + '   ' * (num - 2) + ' *'
+    for i in range(num):
+        print(first_and_last) if (i == 0 or i == (num - 1)) else print(next)
+
 
 square_asterics(10)
 
-#3) вывести табличку множення за допомогою цикла while
+
+# 3) вывести табличку множення за допомогою цикла while
 
 def multiplication_table():
     line = []
-    for i in range(1,10):
-        for j in range(1,10):
-
+    for i in range(1, 10):
+        for j in range(1, 10):
             line.append(f'{(i * j):4}')
         print(' '.join(line))
         print('\n')
         line.clear()
 
-print('\n'*3,'Таблиця множення', '\n'*3, '*' * 50)
+
+print('\n' * 3, 'Таблиця множення', '\n' * 3, '*' * 50)
 multiplication_table()
+
+
+def multiplication_table_():
+    i = 1
+    while (10-i):
+        print(''.join([f'{(i * j):4}' for j in range(1,10)]))
+        i+=1
+
+multiplication_table_()
